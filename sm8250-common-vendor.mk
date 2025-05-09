53d2142f846fa1dd7eb1d2b89249fdfec63407d7
johnmart19 <johnivan19999@gmail.com>
2025-05-09 19:41:58 +0300

    sm8250-common: Setup prebuilt audio HAL
    Drop BOARD_USES_ALSA_AUDIO as it will declare audio.primary.kona, which
    is shipped prebuilt.
    Declare TARGET_PROVIDES_AUDIO_EXTNS as we ship prebuilt liba2dpoffload.
    
    Co-authored-by: Hridaya Prajapati <info.hridayaprajapati@gmail.com>

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -188,16 +188,24 @@ PRODUCT_PACKAGES += \
     libadm \
     libadpcmdec \
     libaudcal \
+    libaudioparsers \
+    libbatterylistener \
     libbluetooth_audio_session_qti \
     libbluetooth_audio_session_qti_2_1 \
     libcapiv2svacnn \
     libcapiv2svarnn \
     libcapiv2vop \
+    libcomprcapture \
+    libdsd2pcm \
+    libexthwplugin \
     libfastcrc \
     libgcs-calwrapper \
     libgcs-ipc \
     libgcs-osal \
     libgcs \
+    libhdmiedid \
+    libhdmipassthru \
+    libhfp \
     liblistensoundmodel2 \
     libmm-hdcpmgr \
     libmmosal_vendor \
@@ -212,6 +220,8 @@ PRODUCT_PACKAGES += \
     libqcodec2_v4l2codec \
     libqtigef \
     libsmwrapper \
+    libsndmonitor \
+    libspkrprot \
     libssrec \
     libwfdcodecv4l2_proprietary \
     libwfdcommonutils_proprietary \
